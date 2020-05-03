8e4635f08f2de3efb9b3a31860eea4444feeb8ad
anand <anand26041997@gmail.com>
2020-05-03 16:55:59 +0530

    DOM beginner

--- DOM/index.js
+++ DOM/index.js
@@ -1,1 +1,26 @@
-alert("HELLO");
+/*document.querySelector("ul").
+lastElementChild.innerHTML="hello";
+ocument.getElementById('#list')
+null
+document.getElementById('list')
+<ul id=​"list">​…​</ul>​
+document.querySelectorAll('li');
+NodeList(3) [li, li, li]
+document.querySelector('li');
+<li>​…​</li>​
+document.querySelectorAll('li')[2].style.color='red';
+"red"
+To add a class at runtime
+document.querySelector('button').classList.add("invisible");
+undefined
+
+
+document.querySelector('a').getAttributeNames;
+ƒ getAttributeNames() { [native code] }
+document.querySelector('a').attributes;
+NamedNodeMap {0: href, href: href, length: 1}
+document.querySelector('a').getAttribute("href");
+"google.com"
+document.querySelector('a').setAttribute("href","bing.com");
+undefined
+*/
